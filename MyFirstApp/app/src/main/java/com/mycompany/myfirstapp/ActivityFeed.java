9c3df18f418cd5ad96feec8ab4dbc9e2139acd8a
888chilly <888chilly@gmail.com>
2015-02-07 07:31:40 -0500

    Camera and Activities Feed are created

---
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/ActivityFeed.java
@@ -0,0 +1,16 @@
+package com.mycompany.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by 888ch_000 on 2/7/2015.
+ */
+public class ActivityFeed extends ActionBarActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
